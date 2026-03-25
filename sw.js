13002aaa4245a303d96cbb04a6a1a00287703a98
Adam Field <personal@mac.lan>
2026-03-25 07:12:07 +0000

    manifest / offline

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'frog-army-v1';
+const ASSETS = [
+  '/frog-army.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/PressStart2P.ttf'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
